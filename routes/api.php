177f55e7012ebad18f5c232161eb2375909fea4c
bndstocco <bndst77@gmail.com>
2025-07-20 00:40:12 -0300

    post

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']); // GET - http://127.0.0.1:8000/api/users?page=1
+Route::get('/users/{user}', [UserController::class, 'show']); // GET http://127.0.0.1:8000/api/users/1
+Route::post('/users',[UserController::class, 'store']); // POST - http://127.0.0.1:8000/api/users
